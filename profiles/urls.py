92603388d11d81b40b79d1ee6193f89f79672bec
Samuel Cattini-Schultz <syriiins@gmail.com>
2019-06-11 08:23:43 +1000

    Refactor routes to be simpler and use query string for details

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path, include
 
-from rest_framework import routers
-
 from profiles import views
 
-router = routers.DefaultRouter()
-router.register(r"scores", views.ScoreViewSet)
-
 urlpatterns = [
-    path(r"", include(router.urls)),
-    path(r"<user_string>/stats/<int:gamemode>", views.GetUserStats.as_view()),
-    path(r"<user_string>/scores/<int:gamemode>", views.GetUserScores.as_view()),
+    path(r"stats/<user_string>/<int:gamemode>", views.GetUserStats.as_view()),
+    path(r"scores", views.ListUserScores.as_view()),
     path(r"api-auth/", include("rest_framework.urls", namespace="rest_framework"))
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -18,58 +18,47 @@ class GetUserStats(APIView):
         """
         Return UserStats based on a user_string and gamemode
         """
-        # TODO: support either user_id or username as search value (frontend should send username if search or non-number url, else id)
-        # temp fix for updating users until rewrite of managers
+        user_id_type = request.query_params.get("user_id_type")
+
         try:
-            # temp only allow username
-            user_stats = UserStats.objects.create_or_update(username=user_string, gamemode=gamemode)
+            if user_id_type == "id":
+                user_stats = UserStats.objects.create_or_update(user_id=user_string, gamemode=gamemode)
+            elif user_id_type == "username":
+                user_stats = UserStats.objects.create_or_update(username=user_string, gamemode=gamemode)
+            else:
+                raise Http404
         except UserStats.DoesNotExist:
             raise Http404
-
-        # try:
-        #     user_stats = self.queryset.get(gamemode=gamemode, user_id=int(user_string))
-        # except (UserStats.DoesNotExist, ValueError):
-        #     try:
-        #         user_stats = self.queryset.get(gamemode=gamemode, user__username__iexact=user_string)
-        #     except UserStats.DoesNotExist:
-        #         raise Http404
 
         serialiser = UserStatsSerialiser(user_stats)
         return Response(serialiser.data)
 
-class GetUserScores(APIView):
+class ListUserScores(APIView):
     """
-    API endpoint for getting Scores
+    API endpoint for Scores
     """
-    queryset = UserStats.objects.non_restricted()
+    queryset = Score.objects.non_restricted()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-    def get(self, request, user_string, gamemode):
+    def get(self, request):
         """
-        Return UserStats based on a user_string and gamemode
+        Return Scores based on a user_id and gamemode
         """
-        try:
-            user_stats = self.queryset.get(gamemode=gamemode, user_id=int(user_string))
-        except (UserStats.DoesNotExist, ValueError):
-            try:
-                user_stats = self.queryset.get(gamemode=gamemode, user__username__iexact=user_string)
-            except UserStats.DoesNotExist:
-                raise Http404
-            
-        scores = user_stats.scores.unique_maps()[:100]
+        user_id = request.query_params.get("user_id")
+        gamemode = request.query_params.get("gamemode")
+        if user_id and gamemode:
+            scores = self.queryset.filter(user_stats__user_id=user_id, user_stats__gamemode=gamemode).unique_maps()[:100]
+        else:
+            scores = self.queryset[:100]
 
         serialiser = ScoreSerialiser(scores, many=True)
         return Response(serialiser.data)
-
-class ScoreViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """
-    API endpoint that allows scores to be viewed.
-    """
-    queryset = Score.objects.non_restricted()
-    serializer_class = ScoreSerialiser
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def create(self, request):
+    
+    def post(self, request):
+        """
+        Add new Scores based on passes user_id, gamemode, beatmap_id
+        """
         data = request.data
-        scores = Score.objects.create_or_update(data["beatmap_id"], data["user_id"], data["gamemode"])
+        scores = self.queryset.model.objects.create_or_update(data["beatmap_id"], data["user_id"], data["gamemode"])
         serialiser = ScoreSerialiser(scores, many=True)
         return Response(serialiser.data)
