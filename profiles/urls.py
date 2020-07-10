bad0f96e7be5e9a9fdbf259abdd0f7a81d19bf89
Samuel Cattini-Schultz <syriiins@gmail.com>
2020-07-11 09:17:23 +1000

    Remove unnecessary raw string literals from url confs

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,9 +7,9 @@ register_converter(GamemodeConverter, "gm")
 register_converter(LeaderboardTypeConverter, "lb_type")
 
 urlpatterns = [
-    path(r"users/<user_string>/stats/<gm:gamemode>", views.GetUserStats.as_view()),
-    path(r"users/<int:user_id>/stats/<gm:gamemode>/scores", views.ListUserScores.as_view()),
-    path(r"users/<int:user_id>/memberships/<lb_type:leaderboard_type>/<gm:gamemode>", views.ListUserMemberships.as_view()),
-    path(r"users/<int:user_id>/invites", views.ListUserInvites.as_view()),
-    path(r"beatmaps/<int:beatmap_id>", views.GetBeatmap.as_view())
+    path("users/<user_string>/stats/<gm:gamemode>", views.GetUserStats.as_view()),
+    path("users/<int:user_id>/stats/<gm:gamemode>/scores", views.ListUserScores.as_view()),
+    path("users/<int:user_id>/memberships/<lb_type:leaderboard_type>/<gm:gamemode>", views.ListUserMemberships.as_view()),
+    path("users/<int:user_id>/invites", views.ListUserInvites.as_view()),
+    path("beatmaps/<int:beatmap_id>", views.GetBeatmap.as_view())
 ]
