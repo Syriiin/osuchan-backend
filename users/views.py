fcdd5cfa6d09702979f88e224e38cb69a7227870
Samuel Cattini-Schultz <syriiins@gmail.com>
2021-09-17 21:55:49 +1000

    Fix missing import

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
