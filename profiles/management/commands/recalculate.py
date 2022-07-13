2dadc96d156e9457571a5b8e3113cdc56e97273c
Samuel Cattini-Schultz <syriiins@gmail.com>
2022-07-13 16:08:25 +1000

    Recalculate memberships in recalculate command

--- profiles/management/commands/recalculate.py
+++ profiles/management/commands/recalculate.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 from common.osu.difficultycalculator import DifficultyCalculator
 from common.osu.enums import Gamemode
+from leaderboards.models import Membership
 from profiles.models import Beatmap, Score, UserStats
 
 
@@ -33,6 +34,12 @@ class Command(BaseCommand):
             )
             return
 
+        self.stdout.write(
+            f"Gamemode: {Gamemode(gamemode).name}\n"
+            f"Difficulty Calculator Engine: {DifficultyCalculator.engine()}\n"
+            f"Difficulty Calculator Version: {DifficultyCalculator.version()}\n"
+        )
+
         # Recalculate beatmaps
         beatmaps = Beatmap.objects.filter(gamemode=gamemode)
 
@@ -49,6 +56,14 @@ class Command(BaseCommand):
         all_user_stats = UserStats.objects.filter(gamemode=gamemode)
 
         self.recalculate_user_stats(all_user_stats)
+
+        # Recalculate memberships
+
+        memberships = Membership.objects.select_related("leaderboard").filter(
+            leaderboard__gamemode=gamemode
+        )
+
+        self.recalculate_memberships(memberships)
 
     def recalculate_beatmap_page(self, page: Iterable[Beatmap], progress_bar: tqdm):
         for beatmap in page:
@@ -163,10 +178,24 @@ class Command(BaseCommand):
             user_stats.recalculate()
             user_stats.save()
 
-            # TODO: update memberships
-
         self.stdout.write(
             self.style.SUCCESS(
                 f"Successfully updated {all_user_stats.count()} user stats"
+            )
+        )
+
+    def recalculate_memberships(self, memberships: QuerySet[Membership]):
+        paginator = Paginator(memberships.order_by("pk"), per_page=2000)
+
+        with tqdm(desc="Memberships", total=memberships.count()) as pbar:
+            for page in paginator:
+                for membership in page:
+                    membership.recalculate()
+                    pbar.update()
+                Membership.objects.bulk_update(page, ["pp"])
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully updated {memberships.count()} memberships"
             )
         )
