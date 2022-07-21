770524a3e3ff697696d5706f61192027759b354a
Samuel Cattini-Schultz <syriiins@gmail.com>
2022-07-21 23:21:30 +1000

    Fix django-debug-toolbar not working in docker

--- local_settings.template.py
+++ local_settings.template.py
@@ -19,8 +19,16 @@ DEBUG = True
 # NOTE: must not be empty when DEBUG is False
 ALLOWED_HOSTS = []
 
-# IPs allowed to use debug toolbar
-INTERNAL_IPS = ["127.0.0.1"]
+# IPs allowed to use debug toolbar (docker makes this a little more complicated)
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#configure-internal-ips
+if DEBUG:
+    import socket  # only if you haven't already imported this
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
+        "127.0.0.1",
+        "10.0.2.2",
+    ]
 
 
 # Logging
