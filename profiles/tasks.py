3e30ade6fd5edc5edc0520b702d62ef0bf66b664
Samuel Cattini-Schultz <syriiins@gmail.com>
2022-07-30 03:18:15 +1000

    Add task to update members of community leaderboards

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -21,6 +21,21 @@ def dispatch_update_all_global_leaderboard_top_members(limit: int = 100):
         for member in members:
             update_user.delay(user_id=member["user_id"], gamemode=leaderboard.gamemode)
 
+
+@shared_task
+def dispatch_update_community_leaderboard_members(
+    leaderboard_id: int, limit: int = 100
+):
+    """
+    Dispatches update_user tasks for all members of a given leaderboard
+    """
+    leaderboard = Leaderboard.community_leaderboards.get(id=leaderboard_id)
+    members = leaderboard.memberships.order_by("-pp")[:limit].values("user_id")
+
+    for member in members:
+        update_user.delay(user_id=member["user_id"], gamemode=leaderboard.gamemode)
+
+
 @shared_task
 @transaction.atomic
 def update_user(user_id=None, username=None, gamemode=Gamemode.STANDARD):
