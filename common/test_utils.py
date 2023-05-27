2906ede341f68933d9f21de57a4f0652ad552913
Samuel Cattini-Schultz <syriiins@gmail.com>
2023-05-27 19:10:52 +1000

    Only use test classes when testing a class

--- common/test_utils.py
+++ common/test_utils.py
@@ -3,25 +3,25 @@ from unittest.mock import Mock, patch
 from common.utils import get_beatmap_path, parse_float_or_none, parse_int_or_none
 
 
-class TestUtils:
-    @patch("common.utils.os.path.isfile", return_value=True)
-    def test_get_beatmap_path_exists(self, isfile_mock: Mock):
-        assert get_beatmap_path("1") == "/app/beatmaps/1"
-        isfile_mock.assert_called_once()
+@patch("common.utils.os.path.isfile", return_value=True)
+def test_get_beatmap_path_exists(self, isfile_mock: Mock):
+    assert get_beatmap_path("1") == "/app/beatmaps/1"
+    isfile_mock.assert_called_once()
 
-    @patch("common.utils.urllib.request.urlretrieve")
-    @patch("common.utils.os.path.isfile", return_value=False)
-    def test_get_beatmap_path_not_exists(
-        self, isfile_mock: Mock, urlretrieve_mock: Mock
-    ):
-        assert get_beatmap_path("1"), "/app/beatmaps/1"
-        isfile_mock.assert_called_once()
-        urlretrieve_mock.assert_called_once()
 
-    def test_parse_int_or_none(self):
-        assert parse_int_or_none("727") == 727
-        assert parse_int_or_none("hello") == None
+@patch("common.utils.urllib.request.urlretrieve")
+@patch("common.utils.os.path.isfile", return_value=False)
+def test_get_beatmap_path_not_exists(self, isfile_mock: Mock, urlretrieve_mock: Mock):
+    assert get_beatmap_path("1"), "/app/beatmaps/1"
+    isfile_mock.assert_called_once()
+    urlretrieve_mock.assert_called_once()
 
-    def test_parse_float_or_none(self):
-        assert parse_float_or_none("727.727") == 727.727
-        assert parse_float_or_none("hello") == None
+
+def test_parse_int_or_none(self):
+    assert parse_int_or_none("727") == 727
+    assert parse_int_or_none("hello") == None
+
+
+def test_parse_float_or_none(self):
+    assert parse_float_or_none("727.727") == 727.727
+    assert parse_float_or_none("hello") == None
