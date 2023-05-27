ba7805cb291a1765df63794b92d059da410a5085
Samuel Cattini-Schultz <syriiins@gmail.com>
2023-05-27 22:01:06 +1000

    Add tests for middleware

---
+++ middleware/test_logging.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock, patch
+
+import pytest
+from django.http import HttpRequest
+from django.test import RequestFactory
+
+from middleware.logging import DiscordErrorLoggingMiddleware
+
+
+class TestDiscordErrorLoggingMiddleware:
+    @pytest.fixture
+    def middleware(self):
+        def get_response(request: HttpRequest):
+            return f"testresponse for {request.path}"
+
+        return DiscordErrorLoggingMiddleware(get_response)
+
+    def test_call(self, middleware: DiscordErrorLoggingMiddleware):
+        rf = RequestFactory()
+        response = middleware(rf.get("/testpath"))
+        assert response == "testresponse for /testpath"
+
+    @patch("middleware.logging.ErrorReporter.report_error")
+    def test_process_exception(
+        self, report_error_mock: Mock, middleware: DiscordErrorLoggingMiddleware
+    ):
+        test_exception = Exception("testexception")
+        rf = RequestFactory()
+        middleware.process_exception(
+            rf.post("/testpath", data={"testkey": "testvalue"}), test_exception
+        )
+        report_error_mock.assert_called_once()
+        assert report_error_mock.call_args.args[0] == test_exception
+        assert (
+            report_error_mock.call_args.kwargs["title"]
+            == f"Exception occured in request `POST /testpath`"
+        )
+        assert "testkey" in report_error_mock.call_args.kwargs["extra_details"]
+        assert "testvalue" in report_error_mock.call_args.kwargs["extra_details"]
