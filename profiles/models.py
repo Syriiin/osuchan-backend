f754b1464ef2a7215a68dfdd2c8705106629f2ff
Samuel Cattini-Schultz <syriiins@gmail.com>
2023-07-04 22:39:00 +1000

    Update django

--- profiles/models.py
+++ profiles/models.py
@@ -242,13 +242,17 @@ class UserStats(models.Model):
         Calculates pp totals (extra pp, nochoke pp) and scores style using unique maps, and returns all scores for UserStats and the scores that need to be added
         """
         # Fetch all scores currently in database and add to new_scores ensuring no duplicate scores
-        database_scores = self.scores.select_related("beatmap").filter(
-            beatmap__status__in=[
-                BeatmapStatus.RANKED,
-                BeatmapStatus.APPROVED,
-                BeatmapStatus.LOVED,
-            ]
-        )
+        try:
+            database_scores = self.scores.select_related("beatmap").filter(
+                beatmap__status__in=[
+                    BeatmapStatus.RANKED,
+                    BeatmapStatus.APPROVED,
+                    BeatmapStatus.LOVED,
+                ]
+            )
+        except ValueError:
+            database_scores = []
+
         database_score_dates = [score.date for score in database_scores]
         scores_to_create = [
             score for score in new_scores if score.date not in database_score_dates
