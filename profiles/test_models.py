84766f18dc836232d6150e76d989333eacb85dcd
Samuel Cattini-Schultz <syriiins@gmail.com>
2024-05-05 00:14:56 +1000

    Add tests for new diffcalc models

--- profiles/test_models.py
+++ profiles/test_models.py
@@ -1,6 +1,15 @@
 import pytest
 
-from profiles.models import Beatmap, OsuUser, Score, UserStats
+from common.osu.difficultycalculator import DifficultyCalculator
+from common.osu.enums import Mods
+from profiles.models import (
+    Beatmap,
+    DifficultyCalculation,
+    OsuUser,
+    PerformanceCalculation,
+    Score,
+    UserStats,
+)
 
 
 @pytest.mark.django_db
@@ -52,3 +61,50 @@ class TestScore:
     def test_update_performance_values(self):
         # TODO: this
         pass
+
+
+@pytest.fixture
+def difficulty_calculation(beatmap: Beatmap):
+    return DifficultyCalculation.objects.create(
+        beatmap=beatmap,
+        mods=Mods.DOUBLETIME + Mods.HIDDEN,
+        calculator_engine="testcalc",
+        calculator_version="v1",
+    )
+
+
+@pytest.mark.django_db
+class TestDifficultyCalculation:
+    def test_calculate_difficulty_values(
+        self, difficulty_calculation: DifficultyCalculation
+    ):
+        difficulty_values = difficulty_calculation.calculate_difficulty_values(
+            DifficultyCalculator
+        )
+        assert len(difficulty_values) == 1
+        assert difficulty_values[0].name == "total"
+        assert difficulty_values[0].value == 8.975730066553297
+
+
+@pytest.mark.django_db
+class TestPerformanceCalculation:
+    @pytest.fixture
+    def performance_calculation(
+        self, score: Score, difficulty_calculation: DifficultyCalculation
+    ):
+        return PerformanceCalculation.objects.create(
+            score=score,
+            difficulty_calculation=difficulty_calculation,
+            calculator_engine="testcalc",
+            calculator_version="v1",
+        )
+
+    def test_calculate_performance_values(
+        self, performance_calculation: PerformanceCalculation
+    ):
+        performance_values = performance_calculation.calculate_performance_values(
+            performance_calculation.score, DifficultyCalculator
+        )
+        assert len(performance_values) == 1
+        assert performance_values[0].name == "total"
+        assert performance_values[0].value == 625.3261007335672
