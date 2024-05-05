e20cd39d194c335a377f39dd3b74f735e0363b4e
Samuel Cattini-Schultz <syriiins@gmail.com>
2024-05-05 17:15:20 +1000

    Uplift diffcalc commands to support multiple calcs

--- profiles/management/commands/calculationstatus.py
+++ profiles/management/commands/calculationstatus.py
@@ -1,7 +1,13 @@
+from typing import Type
+
 from django.core.management.base import BaseCommand
 from django.db.models import QuerySet
 
-from common.osu.difficultycalculator import DifficultyCalculator
+from common.osu.difficultycalculator import (
+    AbstractDifficultyCalculator,
+    DifficultyCalculator,
+    difficulty_calculators,
+)
 from common.osu.enums import Gamemode
 from profiles.models import Beatmap, Score
 
@@ -20,87 +26,113 @@ class Command(BaseCommand):
         # the v2 flag is used to determine whether to use the new difficulty and performance models
         v2 = options["v2"]
 
-        # TODO: iterate over supported gamemodes
-        gamemode = Gamemode.STANDARD
+        for name, difficulty_calculator_class in difficulty_calculators.items():
+            gamemode = difficulty_calculator_class.gamemode()
 
-        self.stdout.write(
-            f"Gamemode: {Gamemode(gamemode).name}\n"
-            f"Difficulty Calculator Engine: {DifficultyCalculator.engine()}\n"
-            f"Difficulty Calculator Version: {DifficultyCalculator.version()}\n"
-        )
-
-        if v2:
-            beatmaps = Beatmap.objects.filter(gamemode=gamemode)
-            outdated_beatmap_count = self.get_outdated_beatmap_count_v2(beatmaps)
-
-            scores = Score.objects.filter(gamemode=gamemode)
-            outdated_score_count = self.get_outdated_score_count_v2(scores)
-        else:
-            beatmaps = Beatmap.objects.filter(gamemode=gamemode)
-            outdated_beatmap_count = self.get_outdated_beatmap_count(beatmaps)
-
-            scores = Score.objects.filter(gamemode=gamemode)
-            outdated_score_count = self.get_outdated_score_count(scores)
-
-        beatmap_count = beatmaps.count()
-        up_to_date_beatmap_count = beatmap_count - outdated_beatmap_count
-
-        score_count = scores.count()
-        up_to_date_score_count = score_count - outdated_score_count
-
-        if up_to_date_beatmap_count == 0:
-            beatmap_output_style = self.style.ERROR
-        elif up_to_date_beatmap_count == beatmap_count:
-            beatmap_output_style = self.style.SUCCESS
-        else:
-            beatmap_output_style = self.style.WARNING
-
-        if up_to_date_score_count == 0:
-            score_output_style = self.style.ERROR
-        elif up_to_date_score_count == score_count:
-            score_output_style = self.style.SUCCESS
-        else:
-            score_output_style = self.style.WARNING
-
-        self.stdout.write(
-            beatmap_output_style(
-                f"Up-to-date Beatmaps: {up_to_date_beatmap_count} / {beatmap_count} ({(up_to_date_beatmap_count / beatmap_count) * 100:.2f}%)"
+            self.stdout.write(
+                "------------------------------------------------------------\n"
+                f"Gamemode: {Gamemode(gamemode).name}\n"
+                f"Difficulty Calculator: {name}\n"
+                f"Difficulty Calculator Engine: {difficulty_calculator_class.engine()}\n"
+                f"Difficulty Calculator Version: {difficulty_calculator_class.version()}\n"
             )
-        )
-        self.stdout.write(
-            score_output_style(
-                f"Up-to-date Scores: {up_to_date_score_count} / {score_count} ({(up_to_date_score_count / score_count) * 100:.2f}%)"
-            )
-        )
 
-    def get_outdated_beatmap_count(self, beatmaps: QuerySet[Beatmap]):
+            if v2:
+                beatmaps = Beatmap.objects.filter(gamemode=gamemode)
+                outdated_beatmap_count = self.get_outdated_beatmap_count_v2(
+                    difficulty_calculator_class, beatmaps
+                )
+
+                scores = Score.objects.filter(gamemode=gamemode)
+                outdated_score_count = self.get_outdated_score_count_v2(
+                    difficulty_calculator_class, scores
+                )
+            else:
+                beatmaps = Beatmap.objects.filter(gamemode=gamemode)
+                outdated_beatmap_count = self.get_outdated_beatmap_count(
+                    difficulty_calculator_class, beatmaps
+                )
+
+                scores = Score.objects.filter(gamemode=gamemode)
+                outdated_score_count = self.get_outdated_score_count(
+                    difficulty_calculator_class, scores
+                )
+
+            beatmap_count = beatmaps.count()
+            up_to_date_beatmap_count = beatmap_count - outdated_beatmap_count
+
+            score_count = scores.count()
+            up_to_date_score_count = score_count - outdated_score_count
+
+            if up_to_date_beatmap_count == 0:
+                beatmap_output_style = self.style.ERROR
+            elif up_to_date_beatmap_count == beatmap_count:
+                beatmap_output_style = self.style.SUCCESS
+            else:
+                beatmap_output_style = self.style.WARNING
+
+            if up_to_date_score_count == 0:
+                score_output_style = self.style.ERROR
+            elif up_to_date_score_count == score_count:
+                score_output_style = self.style.SUCCESS
+            else:
+                score_output_style = self.style.WARNING
+
+            self.stdout.write(
+                beatmap_output_style(
+                    f"Up-to-date Beatmaps: {up_to_date_beatmap_count} / {beatmap_count} ({(up_to_date_beatmap_count / beatmap_count) * 100:.2f}%)"
+                )
+            )
+            self.stdout.write(
+                score_output_style(
+                    f"Up-to-date Scores: {up_to_date_score_count} / {score_count} ({(up_to_date_score_count / score_count) * 100:.2f}%)"
+                )
+            )
+
+    def get_outdated_beatmap_count(
+        self,
+        difficulty_calculator_class: Type[AbstractDifficultyCalculator],
+        beatmaps: QuerySet[Beatmap],
+    ):
         beatmaps_to_recalculate = beatmaps.exclude(
-            difficulty_calculator_engine=DifficultyCalculator.engine(),
-            difficulty_calculator_version=DifficultyCalculator.version(),
+            difficulty_calculator_engine=difficulty_calculator_class.engine(),
+            difficulty_calculator_version=difficulty_calculator_class.version(),
         ).order_by("pk")
 
         return beatmaps_to_recalculate.count()
 
-    def get_outdated_score_count(self, scores: QuerySet[Score]):
+    def get_outdated_score_count(
+        self,
+        difficulty_calculator_class: Type[AbstractDifficultyCalculator],
+        scores: QuerySet[Score],
+    ):
         scores_to_recalculate = scores.exclude(
-            difficulty_calculator_engine=DifficultyCalculator.engine(),
-            difficulty_calculator_version=DifficultyCalculator.version(),
+            difficulty_calculator_engine=difficulty_calculator_class.engine(),
+            difficulty_calculator_version=difficulty_calculator_class.version(),
         ).order_by("pk")
 
         return scores_to_recalculate.count()
 
-    def get_outdated_beatmap_count_v2(self, beatmaps: QuerySet[Beatmap]):
+    def get_outdated_beatmap_count_v2(
+        self,
+        difficulty_calculator_class: Type[AbstractDifficultyCalculator],
+        beatmaps: QuerySet[Beatmap],
+    ):
         beatmaps_to_recalculate = beatmaps.exclude(
-            difficulty_calculations__calculator_engine=DifficultyCalculator.engine(),
-            difficulty_calculations__calculator_version=DifficultyCalculator.version(),
+            difficulty_calculations__calculator_engine=difficulty_calculator_class.engine(),
+            difficulty_calculations__calculator_version=difficulty_calculator_class.version(),
         )
 
         return beatmaps_to_recalculate.count()
 
-    def get_outdated_score_count_v2(self, scores: QuerySet[Score]):
+    def get_outdated_score_count_v2(
+        self,
+        difficulty_calculator_class: Type[AbstractDifficultyCalculator],
+        scores: QuerySet[Score],
+    ):
         scores_to_recalculate = scores.exclude(
-            performance_calculations__calculator_engine=DifficultyCalculator.engine(),
-            performance_calculations__calculator_version=DifficultyCalculator.version(),
+            performance_calculations__calculator_engine=difficulty_calculator_class.engine(),
+            performance_calculations__calculator_version=difficulty_calculator_class.version(),
         )
 
         return scores_to_recalculate.count()
