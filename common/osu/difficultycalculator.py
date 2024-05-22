ee1ad8b5f41ac2dd349cd100b89b359d408aba43
Samuel Cattini-Schultz <syriiins@gmail.com>
2024-05-22 23:13:03 +1000

    Set difficalcy request timeout to 120 seconds

--- common/osu/difficultycalculator.py
+++ common/osu/difficultycalculator.py
@@ -300,7 +300,7 @@ class DifficalcyOsuDifficultyCalculator(AbstractDifficultyCalculator):
     def __init__(self):
         super().__init__()
 
-        self.client = httpx.Client()
+        self.client = httpx.Client(timeout=120.0)
 
     def _close(self):
         self.client.close()
@@ -329,7 +329,7 @@ class DifficalcyOsuDifficultyCalculator(AbstractDifficultyCalculator):
             data = response.json()
         except httpx.HTTPStatusError as e:
             raise CalculationException(
-                f"An error occured in calculating the beatmap {score.beatmap_id}: {e.response.text}"
+                f"An error occured in calculating the beatmap {score.beatmap_id}: [{e.response.status_code}] {e.response.text}"
             ) from e
 
         return Calculation(
@@ -347,7 +347,7 @@ class DifficalcyOsuDifficultyCalculator(AbstractDifficultyCalculator):
             data = response.json()
         except httpx.HTTPStatusError as e:
             raise CalculationException(
-                f"An error occured in calculating the beatmaps: {e.response.text}"
+                f"An error occured in calculating the beatmaps: [{e.response.status_code}] {e.response.text}"
             ) from e
 
         return [
