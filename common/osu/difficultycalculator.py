cb7d31e045731d3dc5cf8d0d119447f2472ce4d7
Samuel Cattini-Schultz <syriiins@gmail.com>
2024-06-08 16:38:35 +1000

    Improve difficalcy error handling

--- common/osu/difficultycalculator.py
+++ common/osu/difficultycalculator.py
@@ -332,9 +332,13 @@ class AbstractDifficalcyDifficultyCalculator(AbstractDifficultyCalculator):
             )
             response.raise_for_status()
             data = response.json()
+        except httpx.HTTPStatusError as e:
+            raise CalculationException(
+                f"An error occured in calculating the beatmap {score.beatmap_id}: {e.request.url} [{e.response.status_code}] {e.response.text}"
+            ) from e
         except httpx.HTTPError as e:
             raise CalculationException(
-                f"An error occured in calculating the beatmap {score.beatmap_id}: [{e.response.status_code}] {e.response.text}"
+                f"An error occured in calculating the beatmap {score.beatmap_id}: {e.request.url} - {e}"
             ) from e
 
         return Calculation(
@@ -350,9 +354,13 @@ class AbstractDifficalcyDifficultyCalculator(AbstractDifficultyCalculator):
             )
             response.raise_for_status()
             data = response.json()
-        except httpx.HTTPError as e:
+        except httpx.HTTPStatusError as e:
             raise CalculationException(
                 f"An error occured in calculating the beatmaps {set(score.beatmap_id for score in scores)}: [{e.response.status_code}] {e.response.text}"
+            ) from e
+        except httpx.HTTPError as e:
+            raise CalculationException(
+                f"An error occured in calculating the beatmaps {set(score.beatmap_id for score in scores)}: {e}"
             ) from e
 
         return [
