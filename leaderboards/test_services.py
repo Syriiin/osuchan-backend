d5b02da0f947fd6910300a7a32ee7e98bc7b2bb6
Samuel Cattini-Schultz <syriiins@gmail.com>
2024-06-10 23:54:24 +1000

    Add test for nochoke leaderboards

--- leaderboards/test_services.py
+++ leaderboards/test_services.py
@@ -1,12 +1,42 @@
 import pytest
 
 from common.osu.enums import Gamemode
-from leaderboards.services import create_membership, update_membership
+from leaderboards.enums import LeaderboardAccessType
+from leaderboards.models import Leaderboard
+from leaderboards.services import (
+    create_leaderboard,
+    create_membership,
+    update_membership,
+)
+from profiles.enums import ScoreSet
+from profiles.models import ScoreFilter
 from profiles.services import fetch_scores
 
 
 @pytest.mark.django_db
 class TestMembershipServices:
+
+    def test_nochoke_leaderboard(self, stub_user_stats, score_filter):
+        leaderboard = create_leaderboard(
+            stub_user_stats.user_id,
+            Leaderboard(
+                gamemode=Gamemode.STANDARD,
+                score_set=ScoreSet.NEVER_CHOKE,
+                access_type=LeaderboardAccessType.PUBLIC,
+                name="test nochoke leaderboard",
+                description="test nochoke leaderboard",
+                icon_url="",
+                allow_past_scores=True,
+                member_count=0,
+                archived=False,
+                notification_discord_webhook_url="",
+                score_filter=ScoreFilter.objects.create(),
+            ),
+        )
+        membership = leaderboard.memberships.first()
+        assert membership.score_count == 4
+        assert membership.pp == 1239.8257144523454
+
     @pytest.fixture
     def membership(self, leaderboard, stub_user_stats):
         return create_membership(leaderboard.id, stub_user_stats.user_id)
